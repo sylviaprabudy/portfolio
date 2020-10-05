89d6923f2f90ab2ad354d7e1fa9d60a0d515d5c7
Sylvia <sylviaprabudy@gmail.com>
2020-10-05 17:53:21 -0500

    Updates

---
+++ precache-manifest.bfea8e3dae3b5d8023ee2c9fbc9d78b2.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4df02d42c5c262a224b67b689c2bf372",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "66e18746141d9291276b",
+    "url": "/portfolio/static/css/main.b4838d26.chunk.css"
+  },
+  {
+    "revision": "57bb24abc334123644c3",
+    "url": "/portfolio/static/js/2.4088cdf0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.4088cdf0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66e18746141d9291276b",
+    "url": "/portfolio/static/js/main.c33b9190.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "5fdc6c77bb513dbe070ef7aa15e175be",
+    "url": "/portfolio/static/media/code-quiz.5fdc6c77.jpg"
+  },
+  {
+    "revision": "cd00ab0b12debf1c1e6a46b126386a78",
+    "url": "/portfolio/static/media/covid-traveling.cd00ab0b.jpg"
+  },
+  {
+    "revision": "37a7e35643cf26a04b3cd4fd7574619c",
+    "url": "/portfolio/static/media/food-festival.37a7e356.jpg"
+  },
+  {
+    "revision": "202256fe40be8c4f4274dc87762b8693",
+    "url": "/portfolio/static/media/git-cookin.202256fe.jpg"
+  },
+  {
+    "revision": "c7fa83e1328cd57731219398ad7eb520",
+    "url": "/portfolio/static/media/git-it-done.c7fa83e1.jpg"
+  },
+  {
+    "revision": "b1d68575e5c6356c4bb21d3ab9dda18a",
+    "url": "/portfolio/static/media/header.b1d68575.jpg"
+  },
+  {
+    "revision": "e1c117735382da7fe6548c4ad343b13a",
+    "url": "/portfolio/static/media/note-taker.e1c11773.jpg"
+  },
+  {
+    "revision": "c3cdde74226177e2475f2b3dbb85fe14",
+    "url": "/portfolio/static/media/password-generator.c3cdde74.jpg"
+  },
+  {
+    "revision": "d900df3734ec678569759adb301d1abc",
+    "url": "/portfolio/static/media/run-buddy.d900df37.jpg"
+  },
+  {
+    "revision": "7ce62a6be3df115132717c30932c7a5c",
+    "url": "/portfolio/static/media/taskmaster-pro.7ce62a6b.jpg"
+  },
+  {
+    "revision": "9e079e3e977dee8f2cee03c9db213fcd",
+    "url": "/portfolio/static/media/tech-blog.9e079e3e.jpg"
+  },
+  {
+    "revision": "80aaba576ca0282d25f975c85ff6c581",
+    "url": "/portfolio/static/media/weather-dashboard.80aaba57.jpg"
+  },
+  {
+    "revision": "d0031df759508f1cf00635e73271ad14",
+    "url": "/portfolio/static/media/work-day-scheduler.d0031df7.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.706f1e5bbd4ec6d26b83e5a2fc0f304b.js"
+  "/portfolio/precache-manifest.bfea8e3dae3b5d8023ee2c9fbc9d78b2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
